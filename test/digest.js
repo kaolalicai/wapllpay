0a0ceedb5557c3c6cbe306e39cf890a0403b1baa
Navy <hxie@kalengo.com>
2015-08-06 14:07:23 +0800

    增加加密解密出错的异常处理，避免引起应用程序退出,完善测试用例

--- test/digest.js
+++ test/digest.js
@@ -54,4 +54,52 @@ describe('digest',function(){
 		var prestr = util.jsonToSearch(para_sort);
 		digest.md5Verify(prestr,sign,key).should.be.ok;
 	});
+	it('#Rsasign()',function(){
+		var key = '201307032000003506';
+		var notifyData = {
+			"oid_partner":"201103171000000000",
+			"dt_order":"20130515094013",
+			"no_order":"2013051500001",
+			"oid_paybill":"2013051613121201",
+			"money_order":"210.97",
+			"result_pay":"SUCCESS",
+			"settle_date":"20130516", "info_order":"用户13958069593购买了3桶羽毛球",
+			"pay_type":"2",
+			"bank_code":"01020000",
+			"sign_type":"MD5", 
+			"sign":"b807649b6a45ef6335aa3fb44930edfb"
+			// "sign_type":"RSA", 
+			// "sign":"ZPZULntRpJwFmGNIVKwjLEF2Tze7bqs60rxQ22CqT5J1UlvGo575QK9z/ +p+7E9cOoRoWzqR6xHZ6WVv3dloyGKDR0btvrdqPgUAoeaX/YOWzTh00vwcQ+HBtX E+vPTfAqjCTxiiSJEOY7ATCF1q7iP3sfQxhS0nDUug1LP3OLk="
+		}
+		var sign = notifyData.sign;
+		var para_filter = util.paraFilter(notifyData);
+		//对待签名参数数组排序
+		var para_sort = util.sortObjectByKey(para_filter);
+		var prestr = util.jsonToSearch(para_sort);
+		digest.Rsasign(prestr,sign,key).should.be.equal('');
+	});
+	it('#Rsaverify()',function(){
+		var key = '201307032000003506';
+		var notifyData = {
+			"oid_partner":"201103171000000000",
+			"dt_order":"20130515094013",
+			"no_order":"2013051500001",
+			"oid_paybill":"2013051613121201",
+			"money_order":"210.97",
+			"result_pay":"SUCCESS",
+			"settle_date":"20130516", "info_order":"用户13958069593购买了3桶羽毛球",
+			"pay_type":"2",
+			"bank_code":"01020000",
+			"sign_type":"MD5", 
+			"sign":"b807649b6a45ef6335aa3fb44930edfb"
+			// "sign_type":"RSA", 
+			// "sign":"ZPZULntRpJwFmGNIVKwjLEF2Tze7bqs60rxQ22CqT5J1UlvGo575QK9z/ +p+7E9cOoRoWzqR6xHZ6WVv3dloyGKDR0btvrdqPgUAoeaX/YOWzTh00vwcQ+HBtX E+vPTfAqjCTxiiSJEOY7ATCF1q7iP3sfQxhS0nDUug1LP3OLk="
+		}
+		var sign = notifyData.sign;
+		var para_filter = util.paraFilter(notifyData);
+		//对待签名参数数组排序
+		var para_sort = util.sortObjectByKey(para_filter);
+		var prestr = util.jsonToSearch(para_sort);
+		digest.Rsaverify(prestr,sign,key).should.be.equal(false);
+	});
 });
